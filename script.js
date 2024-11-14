54c4559b1ee7b80831d7e0d54b503c60a9877905
nyoung90 <musicislovellc@gmail.com>
2024-11-14 04:32:18 +0000

    added js to all 3 cat buttons

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Write your JavaScipt code below this line. 
+const mainTitle = document.getElementById("main-title")
+mainTitle.textContent = "Welcome to Our Cat Adoption Center Zuri!"
+
+const description = document.getElementById("description")
+description.textContent = "Find your purrfect companion!"
+
+const cat1Button = document.getElementById("cat-1-button")
+cat1Button.addEventListener("click", () => {
+    const WhiskersCard = document.getElementById("cat-1")
+    WhiskersCard.remove()
+    alert("Congrats on adopting your new friend Whiskers!")
+})
+
+const cat2Button = document.getElementById("cat-2-button")
+cat2Button.addEventListener("click", () => {
+    const DaiysCard = document.getElementById("cat-2")
+    DaiysCard.remove()
+    alert("Congrats on adopting your new friend Daisy!")
+})
+
+const cat3Button = document.getElementById("cat-3-button")
+cat3Button.addEventListener("click", () => {
+    const TerrysCard = document.getElementById("cat-3")
+    TerrysCard.remove()
+    alert("Congrats on adopting your new friend Terry!")
+})
+
+
+
